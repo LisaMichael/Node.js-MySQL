6757273c64393f5f642cee150155d43a84d11c4d
Lisa <lisa.michael@hotmail.com>
2019-12-15 20:30:22 -0500

    bamazon code is now functioning correctly

--- bamazonManager.js
+++ bamazonManager.js
@@ -65,7 +65,7 @@ function start() {
 
 
 function viewProducts() {
-    console.log("this is the view products code");
+
 
     connection.query('SELECT * FROM products', function (err, res) {
         for (var i = 0; i < res.length; i++) {
@@ -73,21 +73,107 @@ function viewProducts() {
             console.log('Item Id: ' + res[i].item_id + ' '
                 + ' Product: ' + res[i].product_name + ' ' + "DEPARTMENT: " + res[i].department_name + ' Price: $' + res[i].price + ' | ' + ' Available: ' + res[i].stock_quantity);
         }
+        console.log(" ");
+        start();
     })
-start();
-    }
+
+}
 
 
 function lowInventory() {
-            console.log("this is the low inventory code");
+   
+    connection.query("SELECT * FROM Products", function (err, data) {
+        if (err) throw err;
+
+        console.log();
+        console.log("---------- Low Inventory List ----------");
+        console.log("--------------------------------------------------------------------------------------------------");
+        for (var i = 0; i < data.length; i++) {
+            if (data[i].stock_quantity <= 5) {
+                console.log("Item ID: " + data[i].item_id + " | " + "Product Name: " + data[i].product_name + " | " + "Department Name: " + data[i].department_name + " | " + "Price: " + data[i].price + " | " + "Stock Quantity: " + data[i].stock_quantity);
+                console.log("--------------------------------------------------------------------------------------------------");
+            }
         }
+        start();
+    });
+};
 
 function addInventory() {
-            console.log("this is add to inventory code");
-            start();
+    console.log();
+    console.log("---------- Add to Inventory ----------");
+    connection.query("SELECT * FROM Products", function (err, data) {
+        if (err) throw err;
+        var itemsArray = [];
+        for (i = 0; i < data.length; i++) {
+            itemsArray.push(data[i].product_name);
         }
+        inquirer.prompt([{
+            type: "list",
+            name: "products",
+            choices: itemsArray,
+            message: "Which one items would you like to add to Inventory?"
+        },
+        {
+            type: "input",
+            name: "quantity",
+            message: "How many would you like to add the Quantity?",
+            validate: function (value) {
+                if (isNaN(value) === false) {
+                    return true;
+                }
+                return false;
+            }
+        }]).then(function (answer) {
+            var currentQty;
+            for (i = 0; i < data.length; i++) {
+                if (data[i].product_name === answer.products) {
+                    currentQty = data[i].stock_quantity;
+                }
+            }
+            connection.query("UPDATE Products SET ? WHERE ? ", [{ stock_quantity: currentQty + parseInt(answer.quantity) }, { product_name: answer.products }], function (err, data) {
+                if (err) throw err;
+                console.log("The quantity was update!");
+                start();
+            });
+        });
+    });
+};
 
 function newProd() {
-            console.log("this is new Product code");
-        }
+    console.log();
+    console.log("---------- Add to Inventory ----------");
+
+    const questions = [{
+        type: "input",
+        name: "product",
+        message: "Product Name: "
+    },
+    {
+        type: "input",
+        name: "department",
+        message: "Department Name: "
+
+    },
+    {
+        type: "input",
+        name: "price",
+        message: "Price: "
+    },
+    {
+        type: "input",
+        name: "quantity",
+        message: "Stock Quantity: "
+    }];
+
+    inquirer.prompt(questions).then(function (data) {
+        let insertQuery = "INSERT INTO Products (product_name, department_name, price, stock_quantity) VALUES (?, ?, ?, ?)";
+        connection.query(insertQuery, [data.product, data.department, data.price, data.quantity], function (err, data) {
+            console.log("Products Added!");
+        });
+        start();
+    })
+    // }
+
+}
+
 
